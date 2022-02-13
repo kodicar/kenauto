90bb7851a539b92e4888be8135d8260ae967fc3e
dom-inic <dominicnyambane22@gmail.com>
2022-02-13 11:56:08 +0300

    contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    car = models.CharField(max_length=200)
+    car_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    contact_date = models.DateTimeField(auto_now=True, blank=True)
+    user_id = models.IntegerField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Contacts'
+
+    def __str__(self) -> str:
+        return self.name
+
