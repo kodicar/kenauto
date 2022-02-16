e700afb14fde1de1ea08c60d221e0b775f6decf8
dom-inic <dominicnyambane22@gmail.com>
2022-02-16 11:50:06 +0300

    choices

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from . models import Cars
+from .choices import counties, price_choices
 
 # Create your views here.
 def index(request):
