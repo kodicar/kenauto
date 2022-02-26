1a6bddd8748f83a69ce869525dad6d8d41c1ddbe
dom-inic <dominicnyambane22@gmail.com>
2022-02-26 11:24:30 +0300

    whitenoise noisemaking fix

--- kenauto/settings.py
+++ kenauto/settings.py
@@ -63,8 +63,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 ROOT_URLCONF = 'kenauto.urls'
 
 TEMPLATES = [
